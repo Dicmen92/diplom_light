bb0adf8e27e22064002089d0a51db7ead9aeac2a
Dmitriy Babich <babich-d@mail.ru>
2020-09-23 22:10:55 +0600

    made a call to a modal window with a popup-call class

--- js/index.js
+++ js/index.js
@@ -1,1 +1,67 @@
-alert('asdasd');
+//popup 
+
+const togglePopup = () => {
+  const popup = document.querySelector('.popup-call'),
+  popupBtn = document.querySelectorAll('.call-btn');
+
+  //popup анимация
+
+  let animateOpen, 
+      animateClose,
+      count = 0;   
+
+      const animateFuncOpen = () => {
+        animateOpen = requestAnimationFrame(animateFuncOpen);
+        count += 0.05;
+        if (count < 1) {
+          popup.style.opacity = count;
+        } else {
+          cancelAnimationFrame(animateOpen);
+        }
+      };
+
+      const animateFuncClose = () => {
+        animateClose = requestAnimationFrame(animateFuncClose);
+        count -= 0.05;
+        if (count >= 0) {
+          popup.style.opacity = count;
+        } else {
+          cancelAnimationFrame(animateClose);
+          popup.style.display = 'none';
+        }
+      };
+
+  //конец popup анимации
+
+  popupBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      event.preventDefault();      
+      popup.style.display = 'block';
+      animateOpen = requestAnimationFrame(animateFuncOpen);
+
+      if (window.innerWidth <= 768) {
+        cancelAnimationFrame(animateClose); 
+        popup.style.opacity = 1;        
+      }
+    })
+  });
+
+
+  
+  popup.addEventListener('click', (event) => {    
+    let target = event.target;
+
+    if (target.classList.contains('popup-close')) {
+      animateClose = requestAnimationFrame(animateFuncClose);      
+    } else {
+      target = target.closest('.popup-content');  //возвращает ближайшего предка
+
+      if (!target) {
+        animateClose = requestAnimationFrame(animateFuncClose);
+      }
+    }
+  })
+
+};
+
+togglePopup();
