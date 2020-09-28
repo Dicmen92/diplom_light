4a42a97f96aaa146fe1861829af4442d099b8874
Dmitriy Babich <babich-d@mail.ru>
2020-09-28 20:23:23 +0600

    5 completed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 const togglePopup = () => {
   const popup = document.querySelector('.popup-call'),
-  popupBtn = document.querySelectorAll('.call-btn');
+  popupBtn = document.querySelectorAll('.contacts .call-btn');
 
 //-----popup анимация-----
 
@@ -429,3 +429,101 @@ const addSentenceBtn = document.querySelector('.add-sentence-btn'),
   };
 
   togglePopupQuest();
+
+  //конструктор-калькулятор в виде аккордеона
+
+const constructorСalc = () =>{
+  const panelGroup = document.querySelector('.constructor .panel-group'),
+        constructBtn = panelGroup.querySelector('.construct-btn_button'),
+        panelCollapse = panelGroup.querySelectorAll('.panel-collapse'),        
+        panelDefault = panelGroup.querySelectorAll('.panel-default'),
+        linkText = panelGroup.querySelectorAll('.link-text'), 
+        popupDiscount =  document.querySelector('.popup-discount');
+        
+        //-----popup анимация-----
+
+        let animateOpen, 
+        animateClose,
+        count = 0;     
+        const animateFuncOpen = () => {
+          animateOpen = requestAnimationFrame(animateFuncOpen);    
+          count += 0.05;
+          if (count < 1) {
+            popupDiscount.style.opacity = count;
+          } else {
+            cancelAnimationFrame(animateOpen);
+          }
+        };
+
+        const animateFuncClose = () => {
+          animateClose = requestAnimationFrame(animateFuncClose);
+          count -= 0.05;
+          if (count >= 0) {
+            popupDiscount.style.opacity = count;
+          } else {
+            cancelAnimationFrame(animateClose);
+            popupDiscount.style.display = 'none';
+          }
+        };
+
+        //-----конец popup анимации-----
+
+        
+        constructBtn.addEventListener('click', () => {  
+        event.preventDefault();      
+        popupDiscount.style.display = 'block';
+        panelCollapse[panelDefault.length-1].classList.remove('in');
+        panelCollapse[0].classList.add('in');
+        animateFuncOpen();
+
+        if (window.innerWidth <= 768) {
+          animateFuncClose();
+          popupDiscount.style.opacity = 1;        
+        }
+        })        
+
+        popupDiscount.addEventListener('click', (event) => {  
+        let target = event.target;
+
+        if (target.classList.contains('popup-close')) {
+        animateClose = requestAnimationFrame(animateFuncClose);      
+        } else {
+        target = target.closest('.popup-content');  //возвращает ближайшего предка
+
+        if (!target) {
+          animateClose = requestAnimationFrame(animateFuncClose);
+        }
+        }
+        })        
+          
+          panelDefault.forEach((item, i) => {            
+            item.addEventListener('click', (event) => {
+                            
+              let target = event.target;
+              if (target.classList.contains('construct-btn') &&
+              popupDiscount.style.display !== 'block'){                               
+                panelCollapse[i].classList.remove('in');           
+                panelCollapse[i+1].classList.add('in');                              
+              } 
+              
+              if (target.classList.contains('link-text')){
+                panelCollapse[i].classList.add('in');                                
+              }              
+            })
+            item.addEventListener('mouseup', () => {                            
+              linkText.forEach((item, i) => { 
+                let target = event.target;
+                if (!target.classList.contains('link-text')){
+                                                      
+                } else {
+                  panelCollapse[i].classList.remove('in');
+                }
+              })
+              
+            })
+            
+          });         
+
+      };
+  
+  constructorСalc();
