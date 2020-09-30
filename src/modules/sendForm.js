28196ba8aee105e034b70a4505c252d0b0b5a009
Dmitriy Babich <babich-d@mail.ru>
2020-09-30 16:14:41 +0600

    made changes to send the phone to the server

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -49,9 +49,7 @@ const sendForm = () => {
           button[i].setAttribute('disabled', true);          
           button[i].style.cursor = 'pointer';
           telMessage.textContent = phoneMessage;   
-          item.append(telMessage);  
-          console.log(button[i]);
-          console.log(i);
+          item.append(telMessage);            
           //setTimeout(() => telMessage.textContent = '', 5000);
         } else if (elem.value.length === 10 ||
           elem.value.length > 10){
